1d9abafac47623e5e5734aa31a2a010a6ceb0882
Anne <112440960+AKonicki26@users.noreply.github.com>
2023-10-18 13:24:44 -0400

    First Powershell Script

---
+++ Week8/LogToCSV.ps1
@@ -0,0 +1,11 @@
+﻿# Create a PowerShell script that asks user 
+#which logs to export (like in the slides), 
+# then exports to a csv file.
+
+cd C:\Users\champuser\Desktop\
+
+Get-Eventlog -list
+
+$readLog = Read-host -Prompt "Please select a log to output to a CSV file"
+
+Get-Eventlog -LogName $readLog | Export-Csv -NoTypeInformation -Path "C:\Users\champuser\CSI230-01\Week8\$readLog.csv"
