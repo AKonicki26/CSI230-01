4267667b2fca41ca0da61be9491aa7ac2c120bca
AKonicki26 <anne.konicki@mymail.champlain.edu>
2023-11-08 13:58:34 -0500

    Week 10 Done

---
+++ Week10/ServiceLogs.ps1
@@ -0,0 +1,11 @@
+﻿$validResponses = @("all", "stopped", "running")
+
+Do {
+    $input = Read-Host -Prompt "Would you like to see all, running, or stopped services"
+} While (!($input -in $validResponses))
+
+if ($input -eq "all") {
+    Get-Service
+} else {
+    Get-Service | where { $_.Status -ilike $input }
+}
---
+++ Week10/WordCounter.ps1
@@ -0,0 +1,10 @@
+﻿$FilesToLookAt = Get-ChildItem $PSScriptRoot/.. -Recurse -Filter "*.bash" | % {$_.FullName}
+
+
+for ($i = 0; $i -lt $FilesToLookAt.Count; $i++) {
+    $data += Get-Content $FilesToLookAt[$i] | Measure-Object -Word -Line
+}
+
+$data | Measure-Object -Property Words -Minimum -Maximum -Average
+
+$data | select Words, Lines
